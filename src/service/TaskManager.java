64c9fb18fc65a6cc1ff08cdef318ee0eaa252183
Nikolay-Gorshkov <nik_gorshkov_2002@vk.com>
2024-12-18 15:03:19 +0300

    Спринт 9, code style

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -7,31 +7,43 @@ import model.Subtask;
 import java.util.List;
 
 public interface TaskManager {
-    // Методы для задач
+
     void createTask(Task task);
+
     void updateTask(Task task);
+
     Task getTask(int id);
+
     List<Task> getAllTasks();
-    void deleteTask(int id);
+
+    void deleteTask(int id)
+            ;
     void deleteAllTasks();
 
-    // Методы для эпиков
     void createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     Epic getEpic(int id);
+
     List<Epic> getAllEpics();
+
     void deleteEpic(int id);
+
     void deleteAllEpics();
 
-    // Методы для подзадач
     void createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     Subtask getSubtask(int id);
+
     List<Subtask> getAllSubtasks();
+
     void deleteSubtask(int id);
+
     void deleteAllSubtasks();
 
-    // Метод для получения подзадач по ID эпика
     List<Subtask> getSubtasksByEpicId(int epicId);
 
     List<Task> getTasks();
@@ -40,9 +52,7 @@ public interface TaskManager {
 
     List<Subtask> getSubtasks();
 
-    // Методы для истории
     List<Task> getHistory();
 
-    // Методы для приоритетных задач
     List<Task> getPrioritizedTasks();
 }
